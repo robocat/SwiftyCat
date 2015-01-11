71925f1e09a5a2d73c69b87554d7549b53fb6ba5
Ulrik Damm <ulrikdamm@me.com>
2015-01-11 22:20:17 +0100

    Initial Commit

---
+++ SwiftyCat/SwiftyCat.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCat.h
+//  SwiftyCat
+//
+//  Created by Ulrik Damm on 11/01/15.
+//  Copyright (c) 2015 Robocat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCat.
+FOUNDATION_EXPORT double SwiftyCatVersionNumber;
+
+//! Project version string for SwiftyCat.
+FOUNDATION_EXPORT const unsigned char SwiftyCatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCat/PublicHeader.h>
+
+
