a697e4b6a588b7618532939ec593e5bd21f2cdd1
Ulrik Damm <ulrikdamm@me.com>
2015-06-24 11:43:23 +0200

    Dynamic fonts for UILabel, UIButton and UITextView

---
+++ SwiftyCat/DynamicFont.swift
@@ -0,0 +1,69 @@
+//
+//  DynamicFont.swift
+//  SwiftyCat
+//
+//  Created by Ulrik Damm on 24/06/15.
+//  Copyright (c) 2015 Robocat. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 9, *)
+enum DynamicFont {
+	case Title1
+	case Title2
+	case Title3
+	case Headline
+	case Subheadline
+	case Body
+	case Callout
+	case Footnote
+	case Caption1
+	case Caption2
+	
+	var stringValue : String {
+		switch self {
+		case .Title1: return UIFontTextStyleTitle1
+		case .Title2: return UIFontTextStyleTitle2
+		case .Title3: return UIFontTextStyleTitle3
+		case .Headline: return UIFontTextStyleHeadline
+		case .Subheadline: return UIFontTextStyleSubheadline
+		case .Body: return UIFontTextStyleBody
+		case .Callout: return UIFontTextStyleCallout
+		case .Footnote: return UIFontTextStyleFootnote
+		case .Caption1: return UIFontTextStyleCaption1
+		case .Caption2: return UIFontTextStyleCaption2
+		}
+	}
+}
+
+@available(iOS 9, *)
+extension UILabel {
+	func setDynamicFont(textStyle : DynamicFont) {
+		font = UIFont.preferredFontForTextStyle(textStyle.stringValue)
+		
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+		NSNotificationCenter.defaultCenter().addObserverForName(UIContentSizeCategoryDidChangeNotification, object: nil, queue: .mainQueue()) { [weak self] notification in
+			self?.setDynamicFont(textStyle)
+		}
+	}
+}
+
+@available(iOS 9, *)
+extension UITextView {
+	func setDynamicFont(textStyle : DynamicFont) {
+		font = UIFont.preferredFontForTextStyle(textStyle.stringValue)
+		
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+		NSNotificationCenter.defaultCenter().addObserverForName(UIContentSizeCategoryDidChangeNotification, object: nil, queue: .mainQueue()) { [weak self] notification in
+			self?.setDynamicFont(textStyle)
+		}
+	}
+}
+
+@available(iOS 9, *)
+extension UIButton {
+	func setDynamicFont(textStyle : DynamicFont) {
+		titleLabel?.setDynamicFont(textStyle)
+	}
+}
