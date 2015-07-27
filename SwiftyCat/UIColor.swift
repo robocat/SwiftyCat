d771139a3c8232c117cfeedd3ae9e4ef88649c0e
Ulrik Damm <ulrikdamm@me.com>
2015-07-27 11:48:57 +0200

    UIColor hex fixes

--- SwiftyCat/UIColor.swift
+++ SwiftyCat/UIColor.swift
@@ -10,9 +10,9 @@ import UIKit
 
 extension UIColor {
 	public convenience init(hex : UInt32, alpha : CGFloat = 1) {
-		let red = CGFloat(hex & 0x00ff0000) / CGFloat(0xff)
-		let green = CGFloat(hex & 0x0000ff00) / CGFloat(0xff)
-		let blue = CGFloat(hex & 0x000000ff) / CGFloat(0xff)
+		let red = CGFloat(hex & 0x00ff0000 >> 16) / CGFloat(0xff)
+		let green = CGFloat(hex & 0x0000ff00 >> 8) / CGFloat(0xff)
+		let blue = CGFloat(hex & 0x000000ff >> 0) / CGFloat(0xff)
 		self.init(red: red, green: green, blue: blue, alpha: alpha)
 	}
 }
