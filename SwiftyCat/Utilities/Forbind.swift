79de5bfabb906edf8d61724f4f4bf547c36d0c66
Ulrik Damm <ulrikdamm@me.com>
2016-04-13 10:15:11 +0200

    Removed Swift 2.2 warnings

--- SwiftyCat/Utilities/Forbind.swift
+++ SwiftyCat/Utilities/Forbind.swift
@@ -9,13 +9,15 @@
 import Foundation
 import Forbind
 
-public func handleError<T>(onError: ErrorType -> Void)(_ result: Result<T>) -> T? {
-	switch result {
-	case .Error(let error):
-		onError(error)
-		return nil
-	case .Ok(let value):
-		return value
+public func handleError<T>(onError: ErrorType -> Void) -> Result<T> -> T? {
+	return { result in
+		switch result {
+		case .Error(let error):
+			onError(error)
+			return nil
+		case .Ok(let value):
+			return value
+		}
 	}
 }
 
@@ -23,22 +25,26 @@ public func ignoreError<T>(result: Result<T>) -> T? {
 	return handleError({ _ in })(result)
 }
 
-public func pause<T>(time: NSTimeInterval)(value: Result<T>) -> Promise<Result<T>> {
-	let promise = Promise<Result<T>>()
-	
-	dispatch_after(Double(time)) {
-		promise.setValue(value)
+public func pause<T>(time: NSTimeInterval) -> Result<T> -> Promise<Result<T>> {
+	return { value in
+		let promise = Promise<Result<T>>()
+		
+		dispatch_after(Double(time)) {
+			promise.setValue(value)
+		}
+		
+		return promise
 	}
-	
-	return promise
 }
 
-public func dispatch<T>(queue: dispatch_queue_t)(value: Result<T>) -> Promise<Result<T>> {
-	let promise = Promise<Result<T>>()
-	
-	dispatch_async(queue) {
-		promise.setValue(value)
+public func dispatch<T>(queue: dispatch_queue_t) -> Result<T> -> Promise<Result<T>> {
+	return { value in
+		let promise = Promise<Result<T>>()
+		
+		dispatch_async(queue) {
+			promise.setValue(value)
+		}
+		
+		return promise
 	}
-	
-	return promise
 }
